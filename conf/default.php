960e6f66ffa3bf275a7a8baf8e7d51a9c5b9a1c0
Sascha Leib <saschaleib@users.noreply.github.com>
2021-12-10 13:00:48 +0100

    Enable Configuration Options
    The needed files to activate the configuration for template options.

---
+++ conf/default.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Configuration settings for the Ad Hominem template
+ *
+ * @author     Sascha Leib <sascha@leib.be>
+ */
+
+$conf['darkmode']    = 'allow';
+$conf['navtrail']    = 'none';
---
+++ conf/metadata.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Configuration settings for the Ad Hominem template
+ *
+ * @author     Sascha Leib <sascha@leib.be>
+ */
+
+$meta['darkmode'] = array(
+						'multichoice',
+						'_choices' => array ('allow', 'disable'));
+$meta['navtrail'] = array(
+						'multichoice',
+						'_choices' => array ('none', 'text', 'link'));
