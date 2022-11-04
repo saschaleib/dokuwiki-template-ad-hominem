0e9a085966bd021f14a704a024066697dc6d4d2d
saschaleib <sascha@leib.be>
2022-11-04 14:16:36 +0100

    PHP8 fix
    Fixes an error message that appeared in the TOC under PHP8.

--- my_template.php
+++ my_template.php
@@ -462,7 +462,10 @@ function my_toc($prefix = '') {
 				echo "</li>\n";
 			}
 			
-			$href = $it['link'] . ( $it['hid'] == '' ? '' : '#' . $it['hid'] );
+			$href = $it['link'];
+			if	(array_key_exists("hid", $it)) {
+				$href .= '#' . $it['hid'];
+			}
 
 			echo $prefix . str_repeat("\t", $nl*2 + 1) . '<li><a href="' . $href . '">' . htmlentities($it['title']) . "</a>";
 			$level = $nl;
